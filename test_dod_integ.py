2d33c3e7f457d5ce507188e9fc0917669ae2d8f8
Ben Clifford <benc@hawaga.org.uk>
2021-04-08 13:53:13 +0000

    integration work

---
+++ test_dod_integ.py
@@ -0,0 +1,57 @@
+import time
+import pandas as pd
+from blindml.frontend.config.task.task import parse_task_capsule
+from sklearn.metrics import accuracy_score
+from DoD.dod_api import DOD_API
+
+def test_big_small_classifier_quick():
+
+    examples = [["98011", "California"],
+                ["32804", "Florida"]]
+
+    model = get_model_by_example(examples)
+
+    df = pd.DataFrame(data={"PostalCode": ["90210"]})
+    df["PostalCode"] = df["PostalCode"].astype('category')
+    p = model.predict(df)
+
+    print(f"Prediction: {p}")
+
+def get_model_by_example(values):
+    path_model = "../aurum-datadiscovery/test/"
+    separator = ","
+    output_path = ".//testres/"
+    api = DOD_API(path_model, output_path, separator)
+
+    attrs = ["", ""]
+    values = [["98011", "California"], ["32804", "Florida"]]
+
+    api.get_topk_views(attrs, values, 5)
+
+    print("BENC: parsing task capsule")
+    task = parse_task_capsule("dod.jsonnet")
+    print("BENC: finished parsing task capsule")
+    task.search_for_model()
+
+    model = task._auto_sk_model
+
+    X_train, y_train = task._data_set.get_train_data()
+
+    y_pred = model.predict(X_train)
+
+    accuracy = accuracy_score(y_train, y_pred)
+
+    # on such a tiny dataset, this should predict with complete
+    # accuracy on the training data
+    threshold_accuracy = 1.0
+
+    # if accuracy < threshold_accuracy:
+    print(f"Classifier accuracy: {accuracy}")
+
+    print(f"test finished with accuracy {accuracy}")
+
+    return model
+
+if __name__ == "__main__":
+    test_big_small_classifier_quick()
+    print("test successful")
