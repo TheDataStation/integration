b6f39232869542bf571307088cfc9413c956796a
Ben Clifford <benc@hawaga.org.uk>
2021-04-13 13:44:38 +0000

    Add java/gradle build

--- build-docker.sh
+++ build-docker.sh
@@ -4,5 +4,7 @@ rsync -a ~/cqx/c/uchicago/datastation/blindml docker/ --delete
 rsync -a ~/cqx/c/uchicago/datastation/aurum-datadiscovery docker/ --delete
 rsync -a ~/cqx/c/uchicago/datastation/blindml-dod-integ docker/ --delete --exclude=blindml-dod-integ/venv-*
 
+rm docker/aurum-datadiscovery/build -rf
+
 cd docker
 docker build .
