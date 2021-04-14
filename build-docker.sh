ef04eb49bd546a21901a260cc2c3fcfdcd39e833
Ben Clifford <benc@hawaga.org.uk>
2021-04-14 14:37:53 +0000

    check more stuff out of git repos

--- build-docker.sh
+++ build-docker.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -ex
 
-rsync -a ~/cqx/c/uchicago/datastation/blindml docker/ --delete
 rsync -a ~/cqx/c/uchicago/datastation/aurum-datadiscovery docker/ --delete
 rsync -a ~/cqx/c/uchicago/datastation/blindml-dod-integ docker/ --delete --exclude=blindml-dod-integ/venv-*
 
--- docker/import-adventureWork.sh
+++ docker/import-adventureWork.sh
@@ -6,4 +6,8 @@ cd /aurum-datadiscovery/ddprofiler
 
 bash ./run.sh --source=/adventureWork.yaml
 
+cd /aurum-datadiscovery
+
+python3 networkbuildercoordinator.py --opath /model --tpath /adventureWork/
+
 killall --wait java
