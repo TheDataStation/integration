4d236d30dedbc71aff7af07bfac9946c321d4f8e
Ben Clifford <benc@hawaga.org.uk>
2021-04-15 14:30:04 +0000

    updates to build from newly merged other integration repo

--- build-docker.sh
+++ build-docker.sh
@@ -1,9 +1,4 @@
 #!/bin/bash -ex
 
-rsync -a ~/cqx/c/uchicago/datastation/aurum-datadiscovery docker/ --delete
-rsync -a ~/cqx/c/uchicago/datastation/blindml-dod-integ docker/ --delete --exclude=blindml-dod-integ/venv-*
-
-rm docker/aurum-datadiscovery/build -rf
-
 cd docker
 docker build .
