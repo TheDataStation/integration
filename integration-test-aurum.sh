5dcce4ff6af7aab0354b89b5654fb4b3fac647f8
Ben Clifford <benc@hawaga.org.uk>
2021-10-11 09:28:25 +0000

    start services in background and run mostly-successful profiler import

---
+++ integration-test-aurum.sh
@@ -0,0 +1,35 @@
+#!/bin/bash -ex
+
+# this needs to build separately from docker compose because it needs
+# to be built before its users components, and it doesn't look like
+# build order can be specified in docker compose (just startup order?)
+pushd ../aurum-common
+docker build --tag=aurum/aurum-common .
+popd
+
+docker-compose -f ./docker-compose.aurum.yaml build
+
+docker-compose -f ./docker-compose.aurum.yaml up --abort-on-container-exit &
+services_pid=$!
+
+
+# TODO might need to wait for the relevant containers to be up?
+# fake this for now with a sleep
+sleep 60s
+
+# TODO: prep adventurework into /data mountpoint
+
+# Import adventurework dataset:
+# First run ddprofiler
+# Second run nbc
+
+docker-compose -f ./docker-compose.aurum.yaml run profiler
+
+
+# TODO now run the other things from demo
+
+
+docker-compose -f ./docker-compose.aurum.yaml down
+
+wait $services_pid
+echo services exit code $?
